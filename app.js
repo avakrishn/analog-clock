38ee407e94f98f2ae1ab90194cc03afa01d89f90
avakrishn <avakrishn@gmail.com>
2019-05-10 23:05:41 -0700

    add title and digital military time using js

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ window.onload = () => {
 
 const initClock = () => {
     const clock = document.querySelector('.analog');
-    const title = document.querySelector('h1');
+    const title = document.querySelector('.title');
+    const h1 = document.querySelectorAll('h1');
     const clockFace = document.createElement('div');
     const children = ['hour-hand', 'min-hand', 'second-hand'];
-
     title.innerText = 'CURRENT TIME';
-    Object.assign(title.style, {color: 'white', textAlign: 'center'});
+
+    h1.forEach(header => {
+        Object.assign(header.style, {color: 'white', textAlign: 'center'});
+    });    
 
     clockFace.classList.add('clock-face');
     clock.append(clockFace);
@@ -25,30 +28,42 @@ const initClock = () => {
     setInterval(setDate, 1000);// should run every second
 
     function setDate(){ 
+        const digital = document.querySelector('.digital');
         const secondHand = document.querySelector('.second-hand'); 
         const minuteHand = document.querySelector('.min-hand');
         const hourHand = document.querySelector('.hour-hand');
 
         const now = new Date();
 
-        const seconds = now.getSeconds(); // seconds of the current minute
+        let seconds = now.getSeconds(); // seconds of the current minute
         const secondsDeg = ((seconds/ 60) * 360) + 90; // offset the transform: rotate(90deg); CSS rule
         secondHand.style.transform = `rotate(${secondsDeg}deg)`;
 
-        if(secondsDeg === 451){
+        let minute = now.getMinutes();
+        const minuteDeg = ((minute/60) * 360) + 90;
+        minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+
+        let hour = now.getHours();
+        const hourDeg = ((hour/12) * 360) + 90;
+        hourHand.style.transform = `rotate(${hourDeg}deg)`;
+
+        if(secondsDeg === 449){
             secondHand.style.transform = 'all 0s';
 
         }else if(secondsDeg === 90){
             secondHand.style.transform = 'all 0.05s';
         }
 
-        const minute = now.getMinutes();
-        const minuteDeg = ((minute/60) * 360) + 90;
-        minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-
-        const hour = now.getHours();
-        const hourDeg = ((hour/12) * 360) + 90;
-        hourHand.style.transform = `rotate(${hourDeg}deg)`;
+        if(seconds < 10){
+            seconds = "0"+ seconds;
+        }
+        if(minute < 10){
+            minute = "0"+ minute;
+        }
+        if(hour < 10){
+            hour = "0"+ hour;
+        }
+        digital.innerText = `${hour}:${minute}:${seconds}`;
     }
 } 
 
